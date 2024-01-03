3ead58a4b8c32a911328ef5c758a50656d41b30b
Md Sirajul Munir <abc@gmail.com>
2024-01-03 10:15:27 +0600

    all menu added successfully

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../Componants/SectionTitle/SectionTitle";
 import MenuItem from "../../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../Hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data=>{
-            console.log('data length menu ',data.length);
-            const popularItems = data.filter(item => item.category ==='popular');
-            setMenu(popularItems)
-        })
-    },[])
+    const [menu]= useMenu();
+    const popular = menu.filter(item=>item.category === 'popular');
+    // const [menu, setMenu] = useState([]);
+    // useEffect(()=>{
+    //     fetch('menu.json')
+    //     .then(res => res.json())
+    //     .then(data=>{
+    //         console.log('data length menu ',data.length);
+    //         const popularItems = data.filter(item => item.category ==='popular');
+    //         setMenu(popularItems)
+    //     })
+    // },[])
     return (
         <section className="mb-12">
             <SectionTitle heading={"From Our Menu"} subHeading={"Popular Items"}></SectionTitle>
             <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item =><MenuItem key={item._id} item={item}></MenuItem>)
+                    popular.map(item =><MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
         </section>
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,16 +1,42 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import SectionTitle from '../../../Componants/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 const Menu = () => {
+    const [menu]= useMenu();
+    const popular = menu.filter(item=>item.category === 'popular');
+    const desserts = menu.filter(item=>item.category === 'desserts');
+    const soup = menu.filter(item=>item.category === 'soup');
+    const salad = menu.filter(item=>item.category === 'salad');
+    const pizza = menu.filter(item=>item.category === 'pizza');
+    const offered = menu.filter(item=>item.category === 'offered');
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <Cover img={menuImg}></Cover>
-            <h2>This is menu page</h2>
+            <Cover img={menuImg} title={title}></Cover>
+            {/* main cover */}
+            <SectionTitle  subHeading="Don't Miss" heading="Today's offer"></SectionTitle>
+            {/* offered menu item */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items */}
+            <MenuCategory
+            items={desserts}
+            title="Dessert"
+            img={dessertImg}
+            ></MenuCategory>
+            <MenuCategory 
+            items={pizza}
+             title={"pizza"}
+              img={pizzaImg}>
 
+              </MenuCategory>
         </div>
     );
 };
